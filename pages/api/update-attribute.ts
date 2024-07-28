a028a5a8c1b347d552a4017abd3b54c5855afbce
Connor Dirks <cdirks4@me.com>
2024-07-28 02:03:02 -0400

    feat: basic backend

---
+++ pages/api/update-attribute.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { changeAttribute } from '../utils/change-attribute.js';
+import { connectSdk } from '../utils/connect-sdk.js';
+import { getRandomInt } from '../utils/random.js';
+import { Address } from '@unique-nft/sdk/utils';
+
+type ResponseData = {
+  message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  const args = req.body;
+  const [carsCollectionId] = args;
+  const { account, sdk } = await connectSdk();
+  const { address } = req.query;
+  console.log(account);
+  const tokensResult: AccountTokensResult = await sdk.token.accountTokens({
+    collectionId: 3288,
+    address: address,
+  });
+  console.log(tokensResult);
+  const token = await sdk.token.getV2({
+    tokenId: tokensResult.tokens[0].tokenId,
+    collectionId: 3288,
+  });
+
+  console.log(token);
+  const totalScore = token.attributes.find((a) => a.trait_type === 'Total Score').value;
+  console.log(totalScore);
+  //
+  let { nonce } = await sdk.common.getNonce(account);
+  const transactions = [];
+
+  transactions.push(
+    sdk.token.setProperties(
+      {
+        collectionId: 3288,
+        tokenId: token.tokenId,
+        // NOTICE: Attributes stored in "tokenData" property
+        properties: [
+          {
+            key: 'tokenData',
+            value: changeAttribute(token, 'Total Score', args.body.score + totalScore),
+          },
+        ],
+      },
+      { nonce: nonce++ }
+    )
+  );
+
+  const test = await Promise.all(transactions);
+  console.log(test);
+  res.status(200).json({ message: 'Hello from Next.js!' });
+}
